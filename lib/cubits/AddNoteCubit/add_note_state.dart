d3c44fab8cd99c51d7375ad84f8668a1043e58c4
MohamedHA=any1512 <mh861590@gmail.com>
2025-11-19 23:44:24 +0200

    register adaptor

--- lib/cubits/AddNoteCubit/add_note_state.dart
+++ lib/cubits/AddNoteCubit/add_note_state.dart
@@ -5,8 +5,8 @@ sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
 final class AddNoteLoading extends AddNoteState {}
-
 final class AddNoteSuccess extends AddNoteState {}
 final class AddNoteFailure extends AddNoteState {
   final String errorMessage;
-  AddNoteFailure(this.errorMessage);}
+  AddNoteFailure(this.errorMessage);
+}
