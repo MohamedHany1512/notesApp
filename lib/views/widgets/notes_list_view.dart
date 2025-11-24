b45dbc40acbfff54b58465eb0f544cf3d090982a
MohamedHA=any1512 <mh861590@gmail.com>
2025-11-24 21:41:23 +0200

    dsiplay notes part 1

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -1,5 +1,7 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:noteapp/cubits/notes_cubit/notes_cubit.dart';
+import 'package:noteapp/models/note_model.dart';
 import 'package:noteapp/views/widgets/custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
@@ -9,14 +11,21 @@ class NotesListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(itemBuilder: (context,index){
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8 ),
-          child: NoteItem(),
-        );
-      },
-      padding: EdgeInsets.zero,
-      itemCount: 10,
+      child: BlocBuilder<NotesCubit, NotesState>(
+        builder: (context, state) {
+          List<NoteModel> notes =
+              BlocProvider.of<NotesCubit>(context).notes !;
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: NoteItem(),
+              );
+            },
+            padding: EdgeInsets.zero,
+            itemCount: notes.length ,
+          );
+        },
       ),
     );
   }
